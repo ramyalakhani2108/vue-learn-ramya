ad3f865dfaf32de3800153d5ce55578c3943c750
ramyalakhani2108 <lakhani.ramya.u@gmail.com>
2024-12-31 17:13:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api' : {
-        target: 'http://localhost:5001',
+        target: 'https://aesthetic-kelpie-ad2dd2.netlify.app',
         changeOrigin: true,
         rewrite: path => path.replace('/api', '')
       }
