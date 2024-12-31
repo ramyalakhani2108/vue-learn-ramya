2a5b99363c1332672eebd85606ddc059c53f41b3
ramyalakhani2108 <lakhani.ramya.u@gmail.com>
2024-12-31 17:09:24 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router';
 import { createApp } from 'vue';
 import App from './App.vue'
 import toast from 'vue-toastification';
-import '/vue-toastification/dist/index.css';
+import '../node_modules/vue-toastification/dist/index.css';
 const app = createApp(App)
 
 // setting up router
